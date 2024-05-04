7931bed1ba4449816f148620b191ef7e1f2a1459
unknown <amitnale.07@gmail.com>
2024-05-04 13:20:49 +0530

    contact form added and social media link also

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import ContactForm from "../../component/ContactForm/ContactForm";
+import { Container } from "react-bootstrap";
 function Contact(props) {
-  return <div>contact</div>;
+  return (
+    <Container
+      style={{ display: "flex", justifyContent: "center", marginTop: "5%" }}
+    >
+      <div style={{ width: 500 }}>
+        <ContactForm></ContactForm>
+      </div>
+    </Container>
+  );
 }
 
 export default Contact;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./homestyle.css";
+import SocialIcon from "../../component/SocialIcon/SocialIcon";
 
 function Home(props) {
   return (
@@ -10,8 +11,9 @@ function Home(props) {
         <Col className="leftside" lg={8}>
           <h6>HI👋,WELCOME TO MY WORLD</h6>
           <h1>
-            I’m <span>Amit Nale</span> a Developer,Professional Coder..
+            I’m <span>Amit Nale</span> a Developer,Professional Coder...
           </h1>
+          <SocialIcon></SocialIcon>
         </Col>
         <Col lg={4}>
           <div className="octagon">
